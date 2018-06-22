0a926a798f755dfda86479085536a44a637cc0f2
John Lockhart <lockhartjohn527@gmail.com>
2018-06-21 22:49:29 -0700

    Add Stack::Methods

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -1,17 +1,17 @@
 class Stack
     def initialize
-      # create ivar to store stack here!
+      @stack = []
     end
 
     def push(el)
-      # adds an element to the stack
+      @stack.unshift(el)# adds an element to the stack
     end
 
     def pop
-      # removes one element from the stack
+      @stack.shift(el)# removes one element from the stack
     end
 
     def peek
-      # returns, but doesn't remove, the top element in the stack
+      @stack.first # returns, but doesn't remove, the top element in the stack
     end
   end
