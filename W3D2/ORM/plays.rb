af121410a0c5b7072566d98b0f5530bdcf512cd8
lockjohn <lockhartjohn527@gmail.com>
2018-07-03 08:03:48 -0700

    Start to build out ORM again

--- W3D2/ORM/plays.rb
+++ W3D2/ORM/plays.rb
@@ -26,6 +26,36 @@ class Play
     @playwright_id = options['playwright_id']
   end
 
+  def self.find_by_title(title)
+    play = PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT
+      *
+      FROM
+      plays
+      WHERE
+      title = ?
+    SQL
+    return nil unless play.length > 0
+
+    Play.new(play.first)
+  end
+
+  def self.find_by_playwrite(name)
+    playwright = Playwright.find_by_name(name)
+    raise "#{name} not foudn in DB" unless playwright
+
+    plays = PlayDBConnection.instance.execute(<<-SQL,playwright.id)
+    SELECT
+    *
+    FROM
+    plays
+    WHERE
+      playwright_id = ?
+      SQL
+      plays.map{|play| Play.new(play)}
+
+  end
+
   def create
     raise "#{self} already in database" if @id
     PlayDBConnection.instance.execute(<<-SQL, @title, @year, @playwright_id)
@@ -48,4 +78,15 @@ class Play
         id = ?
     SQL
   end
+end
+
+class Playwright
+  attr_accessor :name, :birth_year
+  attr_reader :id
+
+  def self.all
+    data = PlayDPConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum)}
+  end
+
 end
