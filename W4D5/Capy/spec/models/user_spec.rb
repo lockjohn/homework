64758cdba37e740154a0394fc81017e30adf5318
John Lockhart <lockjohn@Johns-MacBook-Air.local>
2018-07-13 08:55:44 -0700

    Stared at furstrating errors, couldnt get anything to work

---
+++ W4D5/Capy/spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  # validations
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
+end
