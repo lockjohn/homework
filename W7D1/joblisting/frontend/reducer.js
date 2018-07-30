ee42a310c69126d3c1f0c2c1388a0d1f682478b5
John Lockhart <lockjohn@Johns-MacBook-Air.local>
2018-07-30 08:58:34 -0700

    add more W7D1 homework

--- W7D1/joblisting/frontend/reducer.js
+++ W7D1/joblisting/frontend/reducer.js
@@ -4,7 +4,15 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  return state; // remove this and fill out the body of the reducer function
+  switch(action.type) {
+    case "SWITCH_LOCATION":
+      return {
+        city: action.city,
+        jobs: action.jobs, //remember action is an POJO, so action.key calls in value from POJO
+      }
+    default:
+    return state;
+  }
 };
 
 export default reducer;
